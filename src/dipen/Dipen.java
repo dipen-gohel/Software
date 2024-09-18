db4b89529af0044a4a1eb3615539b42b11453f04
dipen-gohel <dipengohel123456@gmail.com>
2024-09-18 17:11:29 -0400

    Update Dipen.java
    name

--- src/dipen/Dipen.java
+++ src/dipen/Dipen.java
@@ -16,7 +16,7 @@ public class Dipen {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        System.out.println("dipen");
+        System.out.println("dipen gohel");
     }
     
 }
